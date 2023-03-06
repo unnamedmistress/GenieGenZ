368d0662fd03b2ad2f71f5440168bd6491136759
UnnamedMistress <momchrysti@gmail.com>
2023-03-06 14:11:00 -0500

    heroku deploy

--- server.js
+++ server.js
@@ -31,14 +31,6 @@ app.use((req, res, next) => {
 });
 app.use(cors());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(new URL('./build', import.meta.url)));
-
-  app.get("*", (req, res) => {
-    res.sendFile(new URL('./build/index.html', import.meta.url));
-  });
-}
-
 connect();
 
 // Connect to MongoDB
