8164f1013003cec9dab020db4d3ba75b30a432c0
UnnamedMistress <momchrysti@gmail.com>
2023-03-07 12:41:23 -0500

    no more hashes

--- server.js
+++ server.js
@@ -4,11 +4,8 @@ import mongoose from 'mongoose';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import dotenv from 'dotenv';
-import bcrypt from 'bcrypt';
-import cors from 'cors';
 import User from './models/User.js';
 import connect from './models/connect.js';
-
 
 // Set up environment variables and constants
 const __filename = fileURLToPath(import.meta.url);
@@ -33,7 +30,6 @@ app.use((req, res, next) => {
   res.setHeader('Content-Type', 'application/json');
   next();
 });
-app.use(cors());
 
 connect();
 
@@ -50,7 +46,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', function() {
   console.log('Connected to MongoDB!');
 });
-// POST /api/signup
+
 // POST /api/signup
 app.post('/api/signup', async (req, res) => {
   const { username, password } = req.body;
@@ -65,19 +61,10 @@ console.log('password', password);
       return res.status(400).json({ error: 'Username already exists' });
     }
 
-    // generate a random salt value
-    const salt = await bcrypt.genSalt(10);
-    console.log('Generating salt', salt);
-
-    // hash the password using the salt
-    const hashedPassword = await bcrypt.hash(password.trim(), salt);
-    console.log('Hash of password : ', hashedPassword);
-
     // create new user in the database with the salt and hashed password
     const newUser = new User({
       username,
-      salt,
-      password: hashedPassword,
+      password: password.trim(),
     });
 console.log('New user', newUser);
     await newUser.save();
@@ -89,6 +76,7 @@ console.log('New user', newUser);
   }
 });
 
+// POST /api/login
 app.post('/api/login', async (req, res) => {
   const { username, password } = req.body;
   const user = await User.findOne({ username });
@@ -96,24 +84,12 @@ console.log('user', user);
 console.log('username', username);
 console.log('password', password);
 
-  if (!user) {
+  if (!user || user.password.trim() !== password.trim()) {
     return res.status(401).json({ error: 'Invalid username or password' });
-  }
-
-  const passwordMatch = await bcrypt.compare(password.trim(), user.password.trim());
-  console.log(passwordMatch + ' ' + user.password.trim() + ' ' + password.trim());
-
-  if (!passwordMatch) {
-    console.log(passwordMatch + ' ' + user.password.trim() + ' ' + password.trim());  
-    return res.status(401).json({ error: 'Invalid username or password' });
-  
   }
 
   res.status(200).json({ message: 'Login successful' });
 });
-
-
-
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/index.html'));
