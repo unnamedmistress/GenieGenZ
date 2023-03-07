ced7d96f3384acfa72e361c40d881a7e26f4796d
UnnamedMistress <momchrysti@gmail.com>
2023-03-07 13:17:56 -0500

    typo cause crash fix

--- models/User.js
+++ models/User.js
@@ -1,21 +1,8 @@
-
 import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-});
-userSchema.pre('save', async function(next) {
-  try {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(this.password, salt);
-    this.password = hashedPassword;
-    next();
-    console.log('password hashed' + hashedPassword);
-  } catch (error) {
-    next(error);
-  }
 });
 
 export default mongoose.model('User', userSchema);
